d5c2fdfa1fa60e198801d977a553d16ad9435474
zanetworker <adel.zalok.89@gmail.com>
2019-05-09 22:14:45 +0200

    Add and configure versioning

--- version/cmd/cmd.go
+++ version/cmd/cmd.go
@@ -1,7 +1,9 @@
-package version
+package cmd
 
 import (
 	"fmt"
+
+	"github.com/networkmachinery/networkmachinery-operators/version"
 
 	"github.com/spf13/cobra"
 )
@@ -12,10 +14,14 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of networkmachinery-sflow",
 	Long:  `All software has versions. This is networkmachinery-sflow`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Build Date:", BuildDate)
-		fmt.Println("Git Commit:", GitCommit)
-		fmt.Println("Version:", Version)
-		fmt.Println("Go Version:", GoVersion)
-		fmt.Println("OS / Arch:", OsArch)
+		fmt.Println("Build Date:", version.BuildDate)
+		fmt.Println("Git Commit:", version.GitCommit)
+		fmt.Println("Version:", version.Version)
+		fmt.Println("Go Version:", version.GoVersion)
+		fmt.Println("OS / Arch:", version.OsArch)
 	},
+}
+
+func NewVersionCmd() *cobra.Command {
+	return versionCmd
 }
